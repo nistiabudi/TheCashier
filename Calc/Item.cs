3488f67ceba94fe99eee79c4c22d2bcb62649331
ist003 <ist003@IST003-PC>
2021-10-14 11:13:51 +0800

    Add project files.

---
+++ Calc/Item.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Item
+    {
+        private int id;
+        public string title { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public double subtotal { get; set; }
+        private string type;
+
+        public Item(int id, string title, int quantity, double price, double subtotal, string type)
+        {
+            this.id = id;
+            this.title = title;
+            this.quantity = quantity;
+            this.price = price;
+            this.subtotal = subtotal;
+            this.type = type;
+
+        }
+        public string getTitle()
+        {
+            return title;
+        }
+
+        public int getQuantity()
+        {
+            return quantity;
+        }
+        public string getType()
+        {
+            return type;
+        }
+        public double getPrice()
+        {
+            return price;
+        }
+        public double getSubtotal()
+        {
+            subtotal = price * quantity;
+            return subtotal;
+        }
+    }
+}
